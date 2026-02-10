2d7b65a9c0cb017abf0f2f88a6c6a5f35d4abb89
jieyibi <bijy6@mail2.sysu.edu.cn>
2026-02-10 14:00:05 +0800

    Add TSPTW and TSPDL problem support to OR-Tools

--- baselines/OR-Tools_baseline.py
+++ baselines/OR-Tools_baseline.py
@@ -8,15 +8,17 @@ from six.moves import xrange
 from ortools.constraint_solver import pywrapcp
 from ortools.constraint_solver import routing_enums_pb2
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
+import pickle
 sys.path.insert(0, "..")  # for utils
 from utils import check_extension, load_dataset, save_dataset, run_all_in_pool
 
 SPEED = 1.0
 SCALE = 100000  # EAS uses 1000, while AM uses 100000
 TIME_HORIZON = 3  # the tw_end for the depot node, all vehicles should return to depot before T
+DRAFT_HORIZON = 1000  # the maximal draft limit
 
 
-def create_data_model(depot, loc, demand, capacity, route_limit=None, service_time=None, tw_start=None, tw_end=None, grid_size=1, problem="CVRP"):
+def create_data_model(depot, loc, demand=None, capacity=None, route_limit=None, service_time=None, tw_start=None, tw_end=None, draft=None, grid_size=1, problem="CVRP"):
     """
         Stores the data for the problem
     """
@@ -28,9 +30,17 @@ def create_data_model(depot, loc, demand, capacity, route_limit=None, service_ti
     data['locations'] = [(to_int(x), to_int(y)) for (x, y) in locations]
     data['real_locations'] = locations
     data['num_locations'] = len(data['locations'])  # the number of customer nodes + depot (exclude dummy_depot if any)
-    data['demands'] = [0] + list(demand)
-    data['num_vehicles'] = len(loc)
-    data['vehicle_capacity'] = int(capacity)
+
+    if problem not in ["TSPTW", "TSPDL"]:
+        data['demands'] = [0] + list(demand)
+        data['num_vehicles'] = len(loc)
+        data['vehicle_capacity'] = int(capacity)
+    elif problem == "TSPTW":
+        data['num_vehicles'] = 1
+    elif problem == "TSPDL":
+        data['num_vehicles'] = 1
+        data['demands'] = [int(i) for i in demand]
+        data['draft'] = [int(i) for i in draft]
 
     # For Open Route (e.g., OVRP)
     data['dummy_depot'] = None
@@ -42,6 +52,11 @@ def create_data_model(depot, loc, demand, capacity, route_limit=None, service_ti
         # for depot: [0., 0.] -> Cumul(depot) = 0: vehicle must be at time 0 at depot
         data['time_windows'] = [(to_int(e), to_int(l)) for e, l in zip([0]+tw_start, [0]+tw_end)]
         data['service_time'] = to_int(service_time[0])
+
+    if problem in ["TSPTW"]:
+        # for depot: [0., 0.] -> Cumul(depot) = 0: vehicle must be at time 0 at depot
+        data['time_windows'] = [(to_int(e), to_int(l)) for e, l in zip([0] + tw_start[1:], [0] + tw_end[1:])]
+        data['service_time'] = 0
 
     # For duration limit
     if problem in ["VRPL", "VRPBL", "VRPLTW", "OVRPL", "VRPBLTW", "OVRPBL", "OVRPLTW", "OVRPBLTW"]:
@@ -174,12 +189,41 @@ def create_time_evaluator(data):
     return time_evaluator
 
 
-def add_time_window_constraints(routing, manager, data, time_evaluator_index, grid_size=1):
+def add_draft_constraints(routing, manager, data, demand_evaluator_index, problem="TSPDL"):
+    """
+        Add Draft Limit constraint.
+    """
+    capacity = 'Capacity'
+    routing.AddDimension(
+        demand_evaluator_index,
+        0,  # null capacity slack
+        DRAFT_HORIZON,  # maximum draft limit per vehicle
+        True,  # start cumul to zero
+        capacity)
+    capacity_dimension = routing.GetDimensionOrDie(capacity)
+
+    # Add draft limit constraints for each location
+    for location_idx, draft in enumerate(data['draft']):
+        if location_idx == data['depot'] or location_idx == data['dummy_depot']:
+            continue
+        index = manager.NodeToIndex(location_idx)
+        capacity_dimension.CumulVar(index).SetRange(0, int(draft))
+
+    for vehicle_id in xrange(data['num_vehicles']):
+        index = routing.Start(vehicle_id)
+        # Cumul(depot).SetRange(0, 0) -> vehicle must be empty at depot
+        capacity_dimension.CumulVar(index).SetRange(0, 0)
+
+
+def add_time_window_constraints(routing, manager, data, time_evaluator_index, grid_size=1, time_horizon=None):
     """
         Add Global Span constraint.
     """
     time = 'Time'
-    horizon = int(TIME_HORIZON / grid_size * SCALE + 0.5)
+    if time_horizon is not None:
+        horizon = int(time_horizon / grid_size * SCALE + 0.5)
+    else:
+        horizon = int(TIME_HORIZON / grid_size * SCALE + 0.5)
     routing.AddDimension(
         time_evaluator_index,
         horizon,  # allow waiting time
@@ -224,7 +268,7 @@ def print_solution(data, manager, routing, assignment, problem="CVRP", log_file=
         assert (np.sort(tour)[-len(loc):] == np.arange(len(loc)) + 1).all(), "All nodes must be visited once!"
         loc_with_depot = np.vstack((np.array(depot)[None, :], np.array(loc)))
         sorted_locs = loc_with_depot[np.concatenate(([0], tour, [0]))]
-        if problem in ["CVRP", "VRPB", "VRPL", "VRPTW", "VRPBL", "VRPLTW", "VRPBTW", "VRPBLTW"]:
+        if problem in ["CVRP", "VRPB", "VRPL", "VRPTW", "VRPBL", "VRPLTW", "VRPBTW", "VRPBLTW", "TSPDL"]:
             return np.linalg.norm(sorted_locs[1:] - sorted_locs[:-1], axis=-1).sum()
         elif problem in ["OVRP", "OVRPB", "OVRPL", "OVRPTW", "OVRPBL", "OVRPLTW", "OVRPBTW", "OVRPBLTW"]:  # no need to return to depot
             full_tour = [0] + tour + [0]
@@ -245,10 +289,17 @@ def print_solution(data, manager, routing, assignment, problem="CVRP", log_file=
         distance = 0
         while not routing.IsEnd(index):
             load_var = capacity_dimension.CumulVar(index)
-            plan_output += ' {0} Load({1}) ->'.format(
-                manager.IndexToNode(index),
-                assignment.Value(load_var))
-            route.append(manager.IndexToNode(index))
+            node_idx = manager.IndexToNode(index)
+            if problem == "TSPDL" and 'draft' in data:
+                plan_output += ' {0} Load({1}) <= Draft({2})->'.format(
+                    node_idx,
+                    assignment.Value(load_var),
+                    data["draft"][node_idx])
+            else:
+                plan_output += ' {0} Load({1}) ->'.format(
+                    node_idx,
+                    assignment.Value(load_var))
+            route.append(node_idx)
             previous_index = index
             index = assignment.Value(routing.NextVar(index))
             # distance += routing.GetArcCostForVehicle(previous_index, index, vehicle_id)  # Bugs: always output 0 if given variable index, don't know why
@@ -280,7 +331,52 @@ def print_solution(data, manager, routing, assignment, problem="CVRP", log_file=
     return cost, route[1:]
 
 
-def solve_or_tools_log(directory, name, depot, loc, demand, capacity, route_limit=None, service_time=None, tw_start=None, tw_end=None,
+def print_solution_tsptw(data, manager, routing, assignment, problem="TSPTW", log_file=None):
+    """
+        Only print route, and calculate cost (total distance).
+    """
+
+    def calc_vrp_cost(depot, loc, tour, problem):
+        assert (np.sort(tour)[-len(loc):] == np.arange(len(loc)) + 1).all(), "All nodes must be visited once!"
+        loc_with_depot = np.vstack((np.array(depot)[None, :], np.array(loc)))
+        sorted_locs = loc_with_depot[np.concatenate(([0], tour, [0]))]
+        if problem in ["TSPTW"]:
+            return np.linalg.norm(sorted_locs[1:] - sorted_locs[:-1], axis=-1).sum()
+        else:
+            raise NotImplementedError
+
+    route = []
+    total_distance = 0
+    for vehicle_id in xrange(data['num_vehicles']):
+        if not routing.IsVehicleUsed(vehicle=vehicle_id, assignment=assignment):
+            continue
+        index = routing.Start(vehicle_id)
+        plan_output = 'Route for vehicle {}:\n'.format(vehicle_id)
+        distance = 0
+        while not routing.IsEnd(index):
+            route.append(manager.IndexToNode(index))
+            previous_index = index
+            index = assignment.Value(routing.NextVar(index))
+            from_node, to_node = manager.IndexToNode(previous_index), manager.IndexToNode(index)
+            to_node = to_node if to_node != data['dummy_depot'] else data['depot']
+            distance += data['distance_matrix'][from_node][to_node]
+
+        plan_output += 'Distance of the route: {}\n'.format(distance)
+        if log_file:
+            print(plan_output, file=log_file)
+        total_distance += distance
+
+    # double check
+    cost = calc_vrp_cost(data['real_locations'][0], data['real_locations'][1:], route[1:], problem)
+    if log_file:
+        print('Route: {}'.format(route + [data['depot']]), file=log_file)
+        print('Total Distance of all routes: {} (Routing Error may exist)'.format(total_distance / SCALE), file=log_file)
+        print('Final Result - Cost of the obtained solution: {}'.format(cost), file=log_file)
+
+    return cost, route[1:]
+
+
+def solve_or_tools_log(directory, name, depot, loc, demand, capacity, route_limit=None, service_time=None, tw_start=None, tw_end=None, draft=None,
                        timelimit=3600, grid_size=1, seed=1234, problem="CVRP"):
     """
         OR-Tools to solve VRP variants, Ref to:
@@ -295,7 +391,7 @@ def solve_or_tools_log(directory, name, depot, loc, demand, capacity, route_limi
     log_filename = os.path.join(directory, "{}.or_tools.log".format(name))
 
     data = create_data_model(depot, loc, demand, capacity, route_limit=route_limit, service_time=service_time,
-                             tw_start=tw_start, tw_end=tw_end, grid_size=grid_size, problem=problem)
+                             tw_start=tw_start, tw_end=tw_end, draft=draft, grid_size=grid_size, problem=problem)
 
     # Create the routing index manager
     if problem in ["OVRP", "OVRPB", "OVRPL", "OVRPTW", "OVRPBL", "OVRPLTW", "OVRPBTW", "OVRPBLTW"]:
@@ -314,13 +410,18 @@ def solve_or_tools_log(directory, name, depot, loc, demand, capacity, route_limi
     # routing.SetFixedCostOfAllVehicles(0)
 
     # Add Capacity constraint
-    demand_evaluator_index = routing.RegisterUnaryTransitCallback(partial(create_demand_evaluator(data), manager))
-    add_capacity_constraints(routing, data, demand_evaluator_index, problem=problem)
+    if problem not in ["TSPTW", "TSPDL"]:
+        demand_evaluator_index = routing.RegisterUnaryTransitCallback(partial(create_demand_evaluator(data), manager))
+        add_capacity_constraints(routing, data, demand_evaluator_index, problem=problem)
+    elif problem == "TSPDL":
+        demand_evaluator_index = routing.RegisterUnaryTransitCallback(partial(create_demand_evaluator(data), manager))
+        add_draft_constraints(routing, manager, data, demand_evaluator_index, problem=problem)
 
     # Add Time Window (TW) constraint
-    if problem in ["VRPTW", "VRPBTW", "VRPLTW", "OVRPTW", "VRPBLTW", "OVRPLTW", "OVRPBTW", "OVRPBLTW"]:
+    if problem in ["TSPTW", "VRPTW", "VRPBTW", "VRPLTW", "OVRPTW", "VRPBLTW", "OVRPLTW", "OVRPBTW", "OVRPBLTW"]:
         time_evaluator_index = routing.RegisterTransitCallback(partial(create_time_evaluator(data), manager))
-        add_time_window_constraints(routing, manager, data, time_evaluator_index, grid_size=grid_size)
+        time_horizon = tw_end[0] if tw_end is not None and len(tw_end) > 0 else None
+        add_time_window_constraints(routing, manager, data, time_evaluator_index, grid_size=grid_size, time_horizon=time_horizon)
 
     # Add Duration Limit (L) constraint
     if problem in ["VRPL", "VRPBL", "VRPLTW", "OVRPL", "VRPBLTW", "OVRPBL", "OVRPLTW", "OVRPBLTW"]:
@@ -338,9 +439,13 @@ def solve_or_tools_log(directory, name, depot, loc, demand, capacity, route_limi
     assignment = routing.SolveWithParameters(search_parameters)
     duration = time.time() - start
     if routing.status() not in [1, 2]:
-        print(">> OR-Tools failed to solve instance - Solver status: {}".format(routing.status()))
-        exit(0)
-    cost, route = print_solution(data, manager, routing, assignment, problem=problem, log_file=open(log_filename, 'w'))  # route does not include the first and last node (i.e., depot)
+        print(">> OR-Tools failed to solve instance {} - Solver status: {}".format(name, routing.status()))
+        return None, None, duration
+
+    if problem not in ["TSPTW"]:
+        cost, route = print_solution(data, manager, routing, assignment, problem=problem, log_file=open(log_filename, 'w'))  # route does not include the first and last node (i.e., depot)
+    else:
+        cost, route = print_solution_tsptw(data, manager, routing, assignment, problem=problem, log_file=open(log_filename, 'w'))  # route does not include the first and last node (i.e., depot)
     print("\n".join(["{}".format(r) for r in ([data['depot']] + route + [data['depot']])]), file=open(tour_filename, 'w'))
     save_dataset((route, duration), output_filename, disable_print=True)
 
@@ -351,7 +456,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="OR-Tools baseline")
     parser.add_argument('--problem', type=str, default="VRPBLTW", choices=["CVRP", "OVRP", "VRPB", "VRPL", "VRPTW", "OVRPTW",
                                                                         "OVRPB", "OVRPL", "VRPBL", "VRPBTW", "VRPLTW",
-                                                                        "OVRPBL", "OVRPBTW", "OVRPLTW", "VRPBLTW", "OVRPBLTW"])
+                                                                        "OVRPBL", "OVRPBTW", "OVRPLTW", "VRPBLTW", "OVRPBLTW", "TSPTW", "TSPDL"])
     parser.add_argument("--datasets", nargs='+', default=["../data/VRPBLTW/vrpbltw200_uniform.pkl", ], help="Filename of the dataset(s) to evaluate")
     parser.add_argument("-f", action='store_false', help="Set true to overwrite")
     parser.add_argument("-o", default=None, help="Name of the results file to write")
@@ -362,6 +467,7 @@ if __name__ == '__main__':
     parser.add_argument('-seed', type=int, default=1234, help="random seed")
     parser.add_argument('--offset', type=int, default=0, help="Offset where to start processing")
     parser.add_argument('--results_dir', default='baseline_results', help="Name of results directory")
+    parser.add_argument('--lkh', default=None, help="directory to lkh3")
 
     opts = parser.parse_args()
     assert opts.o is None or len(opts.datasets) == 1, "Cannot specify result filename with more than one dataset"
@@ -382,7 +488,7 @@ if __name__ == '__main__':
 
         def run_func(args):
             directory, name, *args = args
-            depot, loc, demand, capacity, route_limit, service_time, tw_start, tw_end = None, None, None, None, None, None, None, None
+            depot, loc, demand, capacity, route_limit, service_time, tw_start, tw_end, draft, all_loc = None, None, None, None, None, None, None, None, None, None
             if opts.problem in ["CVRP", "OVRP", "VRPB", "OVRPB"]:
                 depot, loc, demand, capacity, *args = args
             elif opts.problem in ["VRPTW", "OVRPTW", "VRPBTW", "OVRPBTW"]:
@@ -391,6 +497,12 @@ if __name__ == '__main__':
                 depot, loc, demand, capacity, route_limit, *args = args
             elif opts.problem in ["VRPLTW", "VRPBLTW", "OVRPLTW", "OVRPBLTW"]:
                 depot, loc, demand, capacity, route_limit, service_time, tw_start, tw_end, *args = args
+            elif opts.problem in ["TSPTW"]:
+                all_loc, service_time, tw_start, tw_end, *args = args
+                depot, loc = all_loc[:1], all_loc[1:]
+            elif opts.problem in ["TSPDL"]:
+                all_loc, demand, draft, *args = args
+                depot, loc = all_loc[:1], all_loc[1:]
             else:
                 raise NotImplementedError
 
@@ -399,7 +511,7 @@ if __name__ == '__main__':
 
             return solve_or_tools_log(
                 directory, name,
-                depot=depot, loc=loc, demand=demand, capacity=capacity, route_limit=route_limit, service_time=service_time, tw_start=tw_start, tw_end=tw_end,
+                depot=depot, loc=loc, demand=demand, capacity=capacity, route_limit=route_limit, service_time=service_time, tw_start=tw_start, tw_end=tw_end, draft=draft,
                 timelimit=opts.timelimit, grid_size=grid_size, seed=opts.seed, problem=opts.problem
             )
 
@@ -417,13 +529,26 @@ if __name__ == '__main__':
         )
 
         costs, tours, durations = zip(*results)  # Not really costs since they should be negative
+        filtered_cost = [x for x,_,_ in results if x is not None]
+        print("Infeasible rate: {}".format((1-len(filtered_cost)/opts.n)*100))
         print(">> Solving {} instances within {:.2f}s using OR-Tools".format(opts.n, time.time() - start_t))
-        print("Average cost: {} +- {}".format(np.mean(costs), 2 * np.std(costs) / np.sqrt(len(costs))))
+        if len(filtered_cost) > 0:
+            print("Average cost: {} +- {}".format(np.mean(filtered_cost), 2 * np.std(filtered_cost) / np.sqrt(len(filtered_cost))))
         print("Average serial duration: {} +- {}".format(np.mean(durations), 2 * np.std(durations) / np.sqrt(len(durations))))
         print("Average parallel duration: {}".format(np.mean(durations) / parallelism))
         print("Calculated total duration: {}".format(timedelta(seconds=int(np.sum(durations) / parallelism))))
 
         results = [(i[0], i[1]) for i in results]
+        if opts.lkh is not None:
+            with open(opts.lkh, 'rb') as file:
+                lkh = pickle.load(file)
+            gaps = []
+            for i in range(opts.n):
+                if results[i][0] is not None and lkh[i][0] is not None:
+                    gap = (results[i][0] - lkh[i][0]) / lkh[i][0]
+                    gaps.append(gap)
+            if len(gaps) > 0:
+                print("GAP: {:.2f}%".format(np.mean(gaps)*100))
         save_dataset(results, out_file)  # [(obj, route), ...]
 
-        # os.system("rm -rf {}".format(target_dir))
+        os.system("rm -rf {}".format(target_dir))
